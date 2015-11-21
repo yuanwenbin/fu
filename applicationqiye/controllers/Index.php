34808e31e2c249933e10c924b7d9ecaa0b721be7
long365 <809692975@qq.com>
2015-11-21 18:01:36 +0800

    ....

--- applicationqiye/controllers/Index.php
+++ applicationqiye/controllers/Index.php
@@ -14,7 +14,7 @@ class Index extends CI_Controller {
 	 */
 	public function menu()
 	{
-		$cate = $this->Qiye_model->menuModel();
+		$cate = $this->Qiye_model->menuModel();	
 		return $cate;
 	}
 	/**
@@ -34,26 +34,59 @@ class Index extends CI_Controller {
 	 */
 	public function listitem()
 	{
-	    
+		$view = array();
+		$cate = $this->menu();
+		$view['cate'] = $cate;	
 	    $id = $this->uri->segment(3,1);
-			echo '<!DOCTYPE html>
-<html>
-<head>
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<title>二级栏目内容待整理中</title>
-<meta name="Keywords" content="" />
-<meta name="Description" content="" />
-<meta name="robots" content="index, follow" />
-<meta name="googlebot" content="index, follow" />
-<link href="/css/style.css" rel="stylesheet" type="text/css" />
-<script src="/js/jquery-1.8.3.min.js" type="text/javascript"></script>
-
-</head>
-<body>';
-			echo '内容有待整理中';
-			echo "</body></html>";
+	    $id = intval($id);
+	    if($id==6)
+	    {
+	    	$this->load->view('chjs', $view);
+	    }elseif($id == 7)
+	    {
+	    	$this->load->view('cityroom', $view);
+	    }else {
+	    	// 二级子栏目
+	    	$subCate = $this->Qiye_model->subCate($id);
+	    	if(!$subCate)
+	    	{
+	    		exit('没有相关内容，请先添加分类');
+	    	}
+	    	$cateName = $this->Qiye_model->subCateModel($id);
+	    	// 文章总数
+	    	$param = array('article_cate_id'=>$id,'article_flag'=>1);
+	    	$totalNumber = $this->Qiye_model->totalRecordModel('fu_article',$param);
+	    	
+	    	// 总页码
+	    	$totalPages = ceil($totalNumber/PAGESIZE);
+	    	$page = $this->uri->segment(4,1);
+	    	if($page < 1)
+	    	{
+	    		$page = 1;
+	    	}elseif($page > $totalPages) {
+	    		$page = $totalPages;
+	    	}
+	    	if($totalPages < 1)
+	    	{
+	    		exit('没有相关内容，请先添加分类');
+	    	}
+	    	$listArticles = $this->Qiye_model->listArticleModel($id,$page,PAGESIZE);
+	    	$view['result']['subCate'] = $subCate;
+	    	$view['result']['totalNumber'] = $totalNumber;
+	    	$view['result']['totalPages'] = $totalPages;
+	    	$view['page'] = $page;
+	    	$view['parent_id'] = $id;
+	    	$view['result']['listArticles'] = $listArticles;
+	    	$view['cateName'] = $cateName;
+	    	// print_r($view); exit;
+	    	$this->load->view('listitem',$view);
+	    	
+	    	
+	    }
 	    //print_r($id);
 	}
+	
+	
 	
 	/**
 	 * 文章详情页
@@ -90,6 +123,48 @@ class Index extends CI_Controller {
 		}else {
 			$this->load->view('details_5',$view);
 		}
+	}
+	
+	/**
+	 * 列表文章详情页
+	 */
+	function listDetail()
+	{
+		$view = array();
+		$cate = $this->menu();
+		$view['cate'] = $cate;	
+	    $id = $this->uri->segment(3,1);
+	    $id = intval($id);
+    	// 二级子栏目
+    	$subCate = $this->Qiye_model->subCate($id);
+    	if(!$subCate)
+    	{
+    		exit('没有相关内容，请先添加分类');
+    	}
+    	$cateName = $this->Qiye_model->subCateModel($id);
+    	$aid = $this->uri->segment(4,0);
+    	if(!$aid)
+    	{
+    		$view['article'] = '没有相关内容';
+    	}else {
+    		$article = $this->Qiye_model->detailsModel($aid);
+    		
+    		if($article)
+    		{
+    			$view['article'] = $article;
+    		}else {
+    			$view['article'] = '没有相关内容';
+    		}
+    	}
+    	$view['result']['subCate'] = $subCate;
+
+    	$view['parent_id'] = $id;
+
+    	$view['cateName'] = $cateName;
+    	//print_r($view); exit;
+    	$this->load->view('listDetail',$view);
+	    	
+	 
 	}
 
 
