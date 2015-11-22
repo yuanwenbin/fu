40d885fae607205ac0854ccd739b0ebb557474ae
good_lin <aobnil@sina.com>
2015-11-22 20:25:15 +0800

    .....

--- applicationqiye/views/listDetail.php
+++ applicationqiye/views/listDetail.php
@@ -111,15 +111,8 @@
 
 	</div>
 	<!-- eof indexContent -->
-
-
-  </div>
-  <!-- eof body -->
-
-  <!-- bof footer -->
-  <div class="footer" style="margin-top:200px;">
 		<!-- bof link -->
-	<div class="friendLinks">
+	<div class="friendLinks"  style="margin-top:200px;">
 		友情链接：
 		<a href="">国家宗教局</a>
 		<a href="">国家宗教局</a>
@@ -129,6 +122,13 @@
 		<a href="">国家宗教局</a>
 	</div>
 	<!-- eof link -->
+
+  </div>
+  <!-- eof body -->
+
+  <!-- bof footer -->
+  <div class="footer">
+
   <p>
 	  <font>宫观介绍</font>&nbsp;|&nbsp;<font>道教典籍</font>&nbsp;|&nbsp;<font>联系我们</font>&nbsp;|&nbsp;<font>道德艺术馆</font>
   </p>
