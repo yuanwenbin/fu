694d6bcc1842f0e29283fc4539a2533f08bb41f1
long365 <809692975@qq.com>
2015-11-23 20:52:59 +0800

    ????

--- applicationadmin/controllers/Curlture.php
+++ applicationadmin/controllers/Curlture.php
@@ -5,7 +5,7 @@ class Curlture extends CI_Controller {
 	function __construct()
 	{
 		parent::__construct();
-		$this->load->model('User_model');
+		$this->load->model('Curlture_model');
 		$this->isLogin();
 	}
 	
@@ -22,14 +22,99 @@ class Curlture extends CI_Controller {
 	}
 	
 	/**
-	 * 友情链接
+	 * 文化列表
 	 */
 	function curlture()
 	{  
-	    if(!hasPerssion($_SESSION['role'], 'all')){
-	        exit('无权限,请点击左栏目操作');
-	    }  
-			echo '内容整理中';
-	  //$this->load->view('userAdd');  
+	    if(!hasPerssion($_SESSION['role'], 'curlture')){
+	    	exit('点击左栏目操作,无权限');
+	    }	
+	    $listArr = array();    
+	    $curltureLists = $this->Curlture_model->curltureList();
+	    /*
+	    if($curltureLists)
+	    {
+	    	foreach($curltureLists as $k=>$v)
+	    	{
+	    		$listArr[$v['curlture_cate']][] = $v;
+	    	}
+	    } */
+	   // $view['listArr'] = $listArr;
+	    $view['listArr'] = $curltureLists;
+	    $curlture[0] = '道教典籍';
+	    $curlture[1] = '道教斋醮';
+	    $curlture[2] = '道教音乐';
+	    $curlture[3] = '道教艺术馆';
+	    $view['curlture'] = $curlture;
+	   $this->load->view('curlture',$view);
+	}
+	
+	/**
+	 * 增加
+	 */
+	function addCurlture()
+	{
+		if(!hasPerssion($_SESSION['role'], 'curlture')){
+			exit('点击左栏目操作,无权限');
+		}
+		$view = array();
+		$curlture[0] = '道教典籍';
+		$curlture[1] = '道教斋醮';
+		$curlture[2] = '道教音乐';
+		$curlture[3] = '道教艺术馆';
+		$view['curlture'] = $curlture;
+		$this->load->view('addCurlture',$view);		
+	}
+	
+	/**
+	 * 增加处理
+	 */
+	function addCurltureDeal()
+	{
+		if(!hasPerssion($_SESSION['role'], 'curlture')){
+			exit('点击左栏目操作,无权限');
+		}
+			
+		// 上传图片
+		$filePic = '';
+		if($_FILES['curlture_pic']['name'])
+		{
+			$filePic = fileUpload($_FILES['curlture_pic']);
+		}
+		$param = array();
+		$param['curlture_headline'] = addslashes($_POST['curlture_headline']);
+		$param['curlture_content'] = addslashes($_POST['content']);
+		$param['curlture_datetime'] = time();
+		$param['curlture_cate'] = intval($_POST['curlture_cate']);
+		$param['curlture_pic'] = $filePic;
+		$param['curlture_seo_title'] = addslashes($_POST['curlture_seo_title']);
+		$param['curlture_seo_keys'] = addslashes($_POST['curlture_seo_keys']);
+		$param['curlture_seo_descritpion'] = addslashes($_POST['curlture_seo_descritpion']);
+		$flag = $this->Curlture_model->addCurltureDealModel('fu_curlture',$param);
+		if($flag)
+		{
+			$this->load->view('success');
+		}else {
+			$this->load->view('failure');
+		}
+	}
+	
+	/**
+	 * 删除处理
+	 */
+	function delCurlture()
+	{
+		$id = intval($this->input->get_post('id'));
+		
+		if(!$id)
+		{
+			$this->load->view('failure');
+		}else {
+			$flag = $this->Curlture_model->delCurltureModel('fu_curlture',array('curlture_id'=>$id));
+			
+	
+			$this->load->view('success');
+			
+		}
 	}
 }
