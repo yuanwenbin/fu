e0e27580333bf0bcb69422c0cd3c41beec4a2168
long365 <809692975@qq.com>
2015-11-23 11:06:25 +0800

    **************

---
+++ applicationadmin/controllers/Curlture.php
@@ -0,0 +1,35 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Curlture extends CI_Controller {
+	function __construct()
+	{
+		parent::__construct();
+		$this->load->model('User_model');
+		$this->isLogin();
+	}
+	
+	/**
+	 * 检测是否登陆
+	 */
+	function isLogin()
+	{
+	    if(!($this->session->userId) || ($this->session->userId) <= 0)
+	    {
+	        header("Location:/Index/login");
+	    }
+	    return true;
+	}
+	
+	/**
+	 * 友情链接
+	 */
+	function curlture()
+	{  
+	    if(!hasPerssion($_SESSION['role'], 'all')){
+	        exit('无权限,请点击左栏目操作');
+	    }  
+			echo '内容整理中';
+	  //$this->load->view('userAdd');  
+	}
+}
