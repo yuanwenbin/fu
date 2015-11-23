b995b899ac7de900fc190804923f6c8e00f7896a
long365 <809692975@qq.com>
2015-11-23 20:55:08 +0800

    *****

---
+++ applicationadmin/views/addCurlture.php
@@ -0,0 +1,148 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+	<meta http-equiv="content-type" content="text/html" />
+	<meta name="author" content="orange" />
+	<meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no"/> 
+	<meta name="renderer" content="webkit">
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+	<title>系统登陆后台</title>
+	<link href="/css/style.css" rel="stylesheet" type="text/css" />
+	<script type="text/javascript" src="/js/jquery-1.8.3.min.js"></script>
+	
+		<style>
+			form {
+				margin: 0;
+			}
+			textarea {
+				display: block;
+			}
+		</style>
+		<link rel="stylesheet" href="/editor/themes/default/default.css" />
+		<script charset="utf-8" src="/editor/kindeditor-min.js"></script>
+		<script charset="utf-8" src="/editor/lang/zh_CN.js"></script>
+		<script>
+			var editor;
+			KindEditor.ready(function(K) {
+				editor = K.create('textarea[name="content"]', {
+					allowFileManager : true
+				});
+				K('input[name=getHtml]').click(function(e) {
+					alert(editor.html());
+				});
+				K('input[name=isEmpty]').click(function(e) {
+					alert(editor.isEmpty());
+				});
+				K('input[name=getText]').click(function(e) {
+					alert(editor.text());
+				});
+				K('input[name=selectedHtml]').click(function(e) {
+					alert(editor.selectedHtml());
+				});
+				K('input[name=setHtml]').click(function(e) {
+					editor.html('<h3>Hello KindEditor</h3>');
+				});
+				K('input[name=setText]').click(function(e) {
+					editor.text('<h3>Hello KindEditor</h3>');
+				});
+				K('input[name=insertHtml]').click(function(e) {
+					editor.insertHtml('<strong>插入HTML</strong>');
+				});
+				K('input[name=appendHtml]').click(function(e) {
+					editor.appendHtml('<strong>添加HTML</strong>');
+				});
+				K('input[name=clear]').click(function(e) {
+					editor.html('');
+				});
+			});
+		</script>	
+	
+</head>
+<body class="roomOpen">
+<h3>增加文化内容</h3>
+<form method="post" action="/Curlture/addCurltureDeal"  enctype="multipart/form-data">
+<table width="100%" border="0" cellpadding="5" cellspacing="0">
+<tr>
+	<td width="15%" align="right">文化详情标题：</td>
+	<td>
+	<input type="text" name="curlture_headline" value="" class="article_title" />
+	</td>
+</tr>
+
+<tr>
+	<td width="15%" align="right">文化分类：</td>
+	<td>
+	<select name="curlture_cate">
+	<?php
+	foreach($curlture as $k=>$v){
+	?>
+	<option value="<?php echo $k;?>">
+	<?php
+	echo $v; ?></option>
+	<?php } ?>
+	</select>
+	</td>	
+</tr>
+
+<tr>
+	<td width="15%" align="right">上传的图片：</td>
+	<td>
+	<input type="file" name="curlture_pic" value="" class="article_title" />&nbsp(道教艺术馆专用)
+	</td>
+</tr>
+
+<tr>
+	<td width="15%" align="right">文化优化标题：</td>
+	<td>
+	<input type="text" name="curlture_seo_title" value="" class="article_title" />
+	</td>
+</tr>
+
+<tr>
+	<td width="15%" align="right">文化优化关键词：</td>
+	<td>
+	<input type="text" name="curlture_seo_keys" value="" class="article_title" />
+	</td>
+</tr>
+
+<tr>
+	<td width="15%" align="right">文化优化描述：</td>
+	<td>
+	<input type="text" name="curlture_seo_descritpion" value="" class="article_title" />
+	</td>
+</tr>
+
+<tr>
+	<td width="15%" align="right">文化详情内容：</td>
+	<td>
+	<textarea name="content" style="width:800px;height:400px;visibility:hidden;"></textarea>
+	</td>
+</tr>
+<tr>
+<td width="15%" align="right">&nbsp;</td>
+<td>&nbsp;</td>
+</tr>
+<tr>
+	<td width="15%" align="right">&nbsp;</td>
+	<td>&nbsp;&nbsp;<a href="/" target="_top">放弃返回</a>&nbsp;&nbsp;<input type="submit" name="submit" value="提交" /></td>
+</tr>
+</table>
+
+</form>
+<script type="text/javascript">
+$(document).ready(function(){
+	$("input[name='submit']").click(function(){
+		var article_title = $("input[name='curlture_headline']").val();
+		var content = $("textarea[name='content']").val();
+		if(article_title == '')
+		{
+			alert("请输入标题！");
+			return false;
+		}
+		return true;
+	});
+	
+});
+</script>
+</body>
+</html>
---
+++ applicationadmin/views/curlture.php
@@ -0,0 +1,59 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+	<meta http-equiv="content-type" content="text/html" />
+	<meta name="author" content="orange" />
+	<meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no"/> 
+	<meta name="renderer" content="webkit">
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+	<title>系统登陆后台</title>
+	<link href="/css/style.css" rel="stylesheet" type="text/css" />
+</head>
+<body class="roomList">
+<?php
+if(!$listArr)
+{
+	echo '没有相关内容 ';
+	echo "<a href='/Curlture/addCurlture'>点击添加</a>";
+	exit;
+}
+?>
+<div class="roomInfosDiv">
+<h3>道教文化列表</h3>
+<table border="0" cellpadding="5" cellspacing="0" width="100%">	
+	<tr class='headerLineBackground'>
+		<th width="20%" align="center">操作</th>
+		<th width="50%" align="center">文化标题</th>
+		<th width="25%" align="center">文化分类</th>
+
+	</tr>
+	<?php
+	foreach($listArr as $k=>$v) { 
+
+	?>
+	<tr class=<?php if($k%2 != 0) echo 'headerLineBackground'; ?>>
+		<td width="20%" align="center">
+		<a href="/Curlture/addCurlture">增加</a>&nbsp;|&nbsp;
+		<a href="/Curlture/delCurlture?id=<?php echo $v['curlture_id']; ?>" onclick="javascript:return sureDel();">删除</a>&nbsp;|&nbsp;
+		<a href="/Curlture/editCurlture?id=<?php echo $v['curlture_id']; ?>">编辑</a>
+		</td>
+		<td width="70%" align="center"><?php echo $v['curlture_headline']; ?></td>
+		<td width="25%" align="center"><?php echo $curlture[$v['curlture_cate']]; ?></td>
+
+	</tr> 	
+
+	<?php } ?>
+</table>
+</div>
+<script type="text/javascript">
+function sureDel()
+{
+	if(confirm("确定要删除吗？"))
+	{
+		return true;
+	}
+	return false;
+}
+</script>
+</body>
+</html>
