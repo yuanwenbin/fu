c1dbd89e2d1cdbd666f7e6c9b4670e3db1c5bf4a
long365 <809692975@qq.com>
2015-11-24 11:25:12 +0800

    <<<<<>>>>>

--- applicationadmin/controllers/Copyright.php
+++ applicationadmin/controllers/Copyright.php
@@ -5,7 +5,7 @@ class Copyright extends CI_Controller {
 	function __construct()
 	{
 		parent::__construct();
-		$this->load->model('User_model');
+		$this->load->model('Curlture_model');
 		$this->isLogin();
 	}
 	
@@ -22,14 +22,69 @@ class Copyright extends CI_Controller {
 	}
 	
 	/**
-	 * 友情链接
+	 * 版本信息
 	 */
 	function copyrightInfo()
 	{  
-	    if(!hasPerssion($_SESSION['role'], 'all')){
+	    if(!hasPerssion($_SESSION['role'], 'copyrightInfo')){
 	        exit('无权限,请点击左栏目操作');
 	    }  
-			echo '内容整理中';
-	  //$this->load->view('userAdd');  
+	    
+		$res = $this->Curlture_model->copyrightInfoModel();
+		if(!$res)
+		{
+			echo '暂时无内容,';
+			echo '<a href="/Copyright/addCopyRight">点击添加内容</a>';
+			exit;
+		}else {
+			$view = array();
+			$view['result']= $res;
+			
+			$this->load->view('copyrightInfo', $view);
+		}
+	  //  
+	}
+	
+	/**
+	 * 增加版权信息
+	 */
+	function addCopyRight()
+	{
+		if(!hasPerssion($_SESSION['role'], 'copyrightInfo')){
+			exit('无权限,请点击左栏目操作');
+		}
+		$this->load->view('addCopyRight');
+	}
+	
+	/**
+	 * 处理增加的信息
+	 */
+	function addCopyrightDeal()
+	{
+		$copy_content = $this->input->get_post('copy_content');
+		if(!trim($copy_content))
+		{
+			$this->load->view('failure');
+		}else {
+			$copy_content = addslashes($copy_content);
+			$res = $this->Curlture_model->addCopyrightDealModel($copy_content);
+			$this->load->view('success');
+		}
+	}
+	
+	/**
+	 * 删除版权信息
+	 */
+	function delCopyRight()
+	{
+		$id = $this->uri->segment(3,1);
+		$id = intval($id);
+		if(!$id)
+		{
+			$this->load->view('failure');
+		}else {
+			$this->Curlture_model->delCurltureModel('fu_copy',array('copy_id'=>$id));
+			$this->load->view('success');
+		}		
 	}
 }
--- applicationadmin/controllers/Links.php
+++ applicationadmin/controllers/Links.php
@@ -5,7 +5,7 @@ class Links extends CI_Controller {
 	function __construct()
 	{
 		parent::__construct();
-		$this->load->model('User_model');
+		$this->load->model('Curlture_model');
 		$this->isLogin();
 	}
 	
@@ -26,10 +26,68 @@ class Links extends CI_Controller {
 	 */
 	function linkList()
 	{  
-	    if(!hasPerssion($_SESSION['role'], 'all')){
+		if(!hasPerssion($_SESSION['role'], 'linkList')){
 	        exit('无权限,请点击左栏目操作');
-	    }  
-			echo '内容整理中';
+	    }	
+		$linkList = $this->Curlture_model->linksList();
+		if(!$linkList)
+		{
+			echo '暂时没有相关内容,';
+			echo '<a href="/Links/addLinks">点击添加</a>';
+		}else {
+			$view = array();
+			$view['result'] = $linkList;
+			$this->load->view('linkList',$view);
+		}
 	  //$this->load->view('userAdd');  
+	}
+	
+	/**
+	 * 增加友情链接
+	 */
+	function addLinks()
+	{
+		if(!hasPerssion($_SESSION['role'], 'linkList')){
+			exit('无权限,请点击左栏目操作');
+		}
+		$this->load->view('addLinks');		
+	}
+	
+	/**
+	 * 增加友情链接处理
+	 */
+	function addLinksDeal()
+	{
+		if(!hasPerssion($_SESSION['role'], 'linkList')){
+			exit('无权限,请点击左栏目操作');
+		}
+		$link_content = addslashes($this->input->get_post('friendLinks'));
+		if(!$link_content)
+		{
+			$this->load->view('failure');
+		}else {
+			$this->Curlture_model->addLinksDealModel($link_content);
+			$this->load->view('success');
+		}
+		
+	}
+	
+	/**
+	 * 删除友情链接
+	 */
+	function delLinks()
+	{
+		if(!hasPerssion($_SESSION['role'], 'linkList')){
+			exit('无权限,请点击左栏目操作');
+		}
+		$id = $this->uri->segment(3,1);
+		$id = intval($id);
+		if(!$id)
+		{
+			$this->load->view('failure');
+		}else {
+			$this->Curlture_model->delCurltureModel('fu_links',array('link_id'=>$id));
+			$this->load->view('success');
+		}
 	}
 }
