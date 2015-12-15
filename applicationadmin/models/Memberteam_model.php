381abb3ad1c5dc721c48d458beba47c6659bb5f4
good_lin <aobnil@sina.com>
2015-12-16 01:32:09 +0800

    ...

--- applicationadmin/models/Memberteam_model.php
+++ applicationadmin/models/Memberteam_model.php
@@ -15,9 +15,11 @@ class Memberteam_model extends CI_Model
 			return $result->result_array();
 		} 
 	}
-
-
-    function userAddDeal($tableName,$param)
+	
+	/**
+	 * 新增加记录
+	 */
+    function memberteamAddModel($tableName,$param)
     {
         $value = '';
         $key = '';
@@ -34,18 +36,26 @@ class Memberteam_model extends CI_Model
     }
     
     /**
-     * 最新id
+     * 查询记录
      */   
-    function newAdminId()
+    function memberteamQueryModel($tableName,$param = '')
     {
-		$sql = "select max(admin_id) as admin_id from fu_admin";
+		 $where = " where 1=1 ";
+		if($param)
+		{
+			foreach($param as $kk=>$vv)
+			{
+				$where .= " and " .$kk." = '" . $vv . "' and ";
+			}
+			$where = substr($where,0,-4);
+		}
+		$sql = "select * from " . $tableName.$where;
 		$result = $this->db->query($sql);
 		if($result->num_rows() < 1)
 		{
 			return '';
 		}else {
-			$idResult= $result->row();
-			return $idResult->admin_id;
+			return $result->result_array();
 		}
 	
     }
