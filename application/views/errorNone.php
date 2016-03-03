976e8fa1410fb07b7bd7d49094836111f65c3c8b
good_lin <aobnil@sina.com>
2016-03-03 08:51:59 +0800

    ////////////

---
+++ application/views/errorNone.php
@@ -0,0 +1,17 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?><!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>出错了</title>
+
+</head>
+<body>
+
+<div>
+没有相关房间，有疑问联系管理员<br />
+<a href="/Index/index">点击返回登陆</a>
+</div>
+</body>
+</html>
